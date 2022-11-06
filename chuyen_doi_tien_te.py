0e1c959084c17b41652493cef96c2efad4773661
nguyetnong <nguyetnong71099@gmail.com>
2022-11-06 14:16:40 +0700

    chuyen doi tien te

---
+++ chuyen_doi_tien_te.py
@@ -0,0 +1,7 @@
+
+tien_can_doi_usd=float(input("nhap so tien usd can doi:"))
+tigia=float(input("nhap ti gia :"))
+vnd=tien_can_doi_usd*tigia
+print("so tien vnd la:",vnd)
+             
+            
